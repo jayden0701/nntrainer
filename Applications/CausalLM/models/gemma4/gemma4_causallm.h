99969172c4bb8d5a162454057cb4c8e08b3bd70c
jayden0701 <jrock.oh@samsung.com>
2026-04-08 00:54:22 +0900

    Add debug

--- Applications/CausalLM/models/gemma4/gemma4_causallm.h
+++ Applications/CausalLM/models/gemma4/gemma4_causallm.h
@@ -54,6 +54,7 @@ protected:
 
   unsigned int HIDDEN_SIZE_PER_LAYER_INPUT = 0;
   unsigned int VOCAB_SIZE_PER_LAYER_INPUT = 0;
+  int NUM_KV_SHARED_LAYERS = 0;
 
 public:
   std::vector<LayerHandle> createAttention(const int layer_id, int seq_len,
