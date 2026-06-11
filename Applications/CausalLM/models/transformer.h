3466a14fd7c7fe0d2d95c745dd31f806a51fac89
jrock-oh <jrock.oh@samsung.com>
2026-06-11 23:31:48 +0900

    causallm: wire embedding sidecar config paths

--- Applications/CausalLM/models/transformer.h
+++ Applications/CausalLM/models/transformer.h
@@ -202,6 +202,22 @@ protected:
   virtual std::pair<Tensor, Tensor> constructModel();
 
   /**
+   * @brief Build common CausalLM embedding layer properties
+   * @param name Layer name
+   * @param in_dim Vocabulary/input dimension
+   * @param out_dim Embedding output dimension
+   * @param weight_dtype Layer weight dtype
+   * @param scale Embedding scale
+   * @param quantized_lut_path Optional sidecar LUT path
+   * @return Layer property strings
+   */
+  std::vector<std::string>
+  buildEmbeddingLayerProperties(const std::string &name, unsigned int in_dim,
+                                unsigned int out_dim,
+                                const std::string &weight_dtype, float scale,
+                                const std::string &quantized_lut_path) const;
+
+  /**
    * @brief Create one Transformer decoder block (norm + attention + residual +
    *        norm + ffn + residual)
    * @param layer_id index of the decoder block
@@ -277,6 +293,8 @@ protected:
   std::string MODEL_TENSOR_TYPE;
   std::string EMBEDDING_DTYPE; /** embedding dtype */
   std::string FC_LAYER_DTYPE;  /** custom_fc_lora */
+  std::string EMBEDDING_FILE_NAME;
+  std::string PLE_FILE_NAME;
 
   unsigned int SLIDING_WINDOW = UINT_MAX;
   unsigned int SLIDING_WINDOW_PATTERN = 5;
